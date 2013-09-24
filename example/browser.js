70df2ccdac249412af2bcbf192d1db25c8495b82
Raynos <raynos2@gmail.com>
2013-09-23 18:24:38 -0700

    comment out wip

--- example/browser.js
+++ example/browser.js
@@ -5,7 +5,7 @@ var window = require("global/window")
 var console = require("console")
 
 var Dom = require("./jsonml/dom.js")
-var Merge = require("./jsonml/merge.js")
+// var Merge = require("./jsonml/merge.js")
 var ObservableArray = require("./lib/observable-array.js")
 var template = require("./template.js")
 var listenMutation = require("./lib/listen-mutation.js")
@@ -17,13 +17,13 @@ var dom = Dom([
     require("./lib/plugin-either.js"),
     require("./lib/plugin-list.js"),
 ])
-var merge = Merge([
-    require("./jsonml/plugins/loose.js"),
-    require("./jsonml/plugins/fragment.js"),
-    require("./jsonml/plugins/observ.js"),
-    require("./lib/plugin-either.js"),
-    require("./lib/plugin-list.js"),
-])
+// var merge = Merge([
+//     require("./jsonml/plugins/loose.js"),
+//     require("./jsonml/plugins/fragment.js"),
+//     require("./jsonml/plugins/observ.js"),
+//     require("./lib/plugin-either.js"),
+//     require("./lib/plugin-list.js"),
+// ])
 
 var state = JSONGlobals("model")
 var model = window.model = Object.keys(state).reduce(function (acc, key) {
@@ -38,11 +38,11 @@ console.log("RENDER")
 var elem = dom(template(model))
 document.body.appendChild(elem)
 
-console.log("MERGE")
-var mainElem = document.getElementById("main")
-merge(template(model), {
-    root: mainElem
-})
+// console.log("MERGE")
+// var mainElem = document.getElementById("main")
+// merge(template(model), {
+//     root: mainElem
+// })
 
 
 listenMutation(document.body, function (delta) {
