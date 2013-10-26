ef12f63fcc23f8ba5ef24a5636d5954a8c2cdbdc
Raynos <raynos2@gmail.com>
2013-10-26 16:44:15 -0700

    more delegator

--- examples/lib/dom-delegator.js
+++ examples/lib/dom-delegator.js
@@ -1,12 +1,30 @@
 var document = require("global/document")
 var EventEmitter = require("events").EventEmitter
+var emitterOn = EventEmitter.prototype.on
 
 module.exports = Delegator
 
-function Delegator(rootNode) {
+function Delegator(rootNode, opts) {
+    if (rootNode && !rootNode.nodeName) {
+        opts = rootNode
+        rootNode = null
+    }
+
     rootNode = rootNode || document
+    opts = opts || {}
 
     var emitter = new EventEmitter()
+    var eventsTable = {}
+
+    emitter.on = emitter.addListener = on
+
+    if (opts.walk !== false) {
+        
+    }
 
     return emitter
+
+    function on() {
+
+    }
 }
